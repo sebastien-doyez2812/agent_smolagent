330db3417ee7b042ec05ec9a56cddea6432d72ab
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-18 16:57:21 -0400

    manager_agent and visuaisation

--- multisystems_agent_smolagents.py
+++ multisystems_agent_smolagents.py
@@ -4,7 +4,7 @@ from PIL import Image
 from typing import Optional, Tuple
 from smolagents import tool
 from smolagents import CodeAgent, DuckDuckGoSearchTool, VisitWebpageTool, LiteLLMModel
-
+from smolagents.utils import encode_image_base64, make_image_url
 
 model = LiteLLMModel(
     model_id="ollama_chat/llama3.2",
@@ -59,21 +59,86 @@ def calculate_cargo_travel_time(
     return round(flight_time, 2)
 
 
-agent = CodeAgent(
-    model=model,
-    tools=[DuckDuckGoSearchTool(), VisitWebpageTool(), calculate_cargo_travel_time],
-    additional_authorized_imports=["pandas"],
-    max_steps=20,
+def check_reasoning_and_plot(final_answer, agent_memory):
+    multimodal_model = LiteLLMModel(model_id= "ollama_chat/llava")
+    filepath = "saved_map.png"
+    assert os.path.exists(filepath)
+    image = Image.open(filepath)
+    prompt = (
+        f"Here is a user-given task and the agent steps: {agent_memory.get_succinct_steps()}. Now here is the plot that was made."
+        "Please check that the reasoning process and plot are correct: do they correctly answer the given task?"
+        "First list reasons why yes/no, then write your final decision: PASS in caps lock if it is satisfactory, FAIL if it is not."
+        "Don't be harsh: if the plot mostly solves the task, it should pass."
+        "To pass, a plot should be made using px.scatter_map and not any other method (scatter_map looks nicer)."
+    )
+
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {"url": make_image_url(encode_image_base64(image))},
+                },
+            ],
+        }
+    ]
+
+    output = multimodal_model(messages).content
+    print("Feedback: ", output)
+    if "FAIL" in output:
+        raise Exception(output)
+    return True
+
+
+# agent = CodeAgent(
+#     model=model,
+#     tools=[DuckDuckGoSearchTool(), VisitWebpageTool(), calculate_cargo_travel_time],
+#     additional_authorized_imports=["pandas"],
+#     max_steps=20,
+# )
+
+web_agent = CodeAgent(
+    model = model,
+    tools= [DuckDuckGoSearchTool(), VisitWebpageTool(), calculate_cargo_travel_time],
+    name = "web_agent",
+    description = "Browse the web to find information",
+    verbosity_level = 0,
+    max_steps = 10
 )
 
 
-task = """
-Step 1: Find a list of Batman filming locations and their coordinates.
-Step 2: Calculate the travel time from (40.7128, -74.0060) to each filming location.
-Step 3: Return the results as a Pandas DataFrame.
-"""
+manager_agent = CodeAgent(
+    model=LiteLLMModel( model_id= "ollama_chat/deepseek-r1", max_tokens=8096),
+    tools=[calculate_cargo_travel_time],
+    managed_agents=[web_agent],
+    additional_authorized_imports=[
+        "geopandas",
+        "plotly",
+        "shapely",
+        "json",
+        "pandas",
+        "numpy",
+    ],
+    planning_interval=5,
+    verbosity_level=2,
+    final_answer_checks=[check_reasoning_and_plot],
+    max_steps=15,
+)
 
-# task = """Find all Batman filming locations in the world, calculate the time to transfer via cargo plane to here (we're in Gotham, 40.7128° N, 74.0060° W), and return them to me as a pandas dataframe.
-# Also give me some supercar factories with the same cargo plane transfer time."""
 
-print(agent.run(task))
+manager_agent.visualize()
+# task = """
+# Step 1: Find a list of Batman filming locations and their coordinates.
+# Step 2: Calculate the travel time from (40.7128, -74.0060) to each filming location.
+# Step 3: Return the results as a Pandas DataFrame.
+# """
+
+# # task = """Find all Batman filming locations in the world, calculate the time to transfer via cargo plane to here (we're in Gotham, 40.7128° N, 74.0060° W), and return them to me as a pandas dataframe.
+# # Also give me some supercar factories with the same cargo plane transfer time."""
+
+# print(agent.run(task))
