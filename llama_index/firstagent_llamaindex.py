db07e8a2341f6befc7c762c3d08d2bd40016f08e
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-19 21:24:47 -0400

    run locally with ollama

--- llama_index/firstagent_llamaindex.py
+++ llama_index/firstagent_llamaindex.py
@@ -1,4 +1,5 @@
 from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
+from llama_index.llms.ollama import Ollama
 from dotenv import load_dotenv
 import os
 
@@ -7,12 +8,16 @@ import os
 load_dotenv(".env")
 hf_token = os.getenv("HF_TOKEN")
 
-llm = HuggingFaceInferenceAPI(
-    model_name = "Qwen/Qwen2.5-Coder-32B-Instruct",
-    temperature = 0.7,
-    max_tokens = 100,
-    token = hf_token
-)
+# HF Inference API:
+# llm = HuggingFaceInferenceAPI(
+#     model_name = "Qwen/Qwen2.5-Coder-32B-Instruct",
+#     temperature = 0.7,
+#     max_tokens = 100,
+#     token = hf_token
+# )
+ 
 
+# Local LLMs with Ollama:
+llm = Ollama(model = "gemma") 
 response = llm.complete("Hi, how are you?")
 print(response)
