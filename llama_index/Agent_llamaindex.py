895dd56ea97fff165a20d384e7c66380de4077a0
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-24 15:47:05 -0400

    Create a Tool calling agent using LlamaIndex and Ollama

---
+++ llama_index/Agent_llamaindex.py
@@ -0,0 +1,27 @@
+from llama_index.llms.ollama import Ollama
+from llama_index.core.agent.workflow import AgentWorkflow
+from llama_index.core.tools import FunctionTool
+from llama_index.core.workflow import Context
+import asyncio
+
+def multiply(a:int , b: int) -> int:
+    return a *b
+
+llm = Ollama(model= "qwen2.5", is_function_calling_model= True)
+
+agent = AgentWorkflow.from_tools_or_functions([
+    FunctionTool.from_defaults(multiply)
+], 
+llm= llm)
+
+
+
+async def main():
+    ctx = Context(agent)
+    questions = ["what is 2 times 2?", "Hey I am Seb!", "What's my name?"]
+    for question in questions:
+        response = await agent.run(question, ctx = ctx)
+        print(response)
+
+
+asyncio.run(main())
