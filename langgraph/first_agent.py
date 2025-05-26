8b312572f4d508f0a08a5081a1623c6ca8085a2d
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-26 18:20:13 -0400

    add nodes

---
+++ langgraph/first_agent.py
@@ -0,0 +1,20 @@
+from typing_extensions import TypedDict
+
+
+
+class State(TypedDict):
+    graph_state: str
+
+
+# Define the nodes as functions:
+def node_1(state):
+    print("---Node 1---")
+    return {"graph_state": state['graph_state'] +" I am"}
+
+def node_2(state):
+    print("---Node 2---")
+    return {"graph_state": state['graph_state'] +" happy!"}
+
+def node_3(state):
+    print("---Node 3---")
+    return {"graph_state": state['graph_state'] +" sad!"}
