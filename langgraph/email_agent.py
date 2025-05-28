681dd6f04acba285ab9d9be2131173996ec1af6a
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-27 21:48:23 -0400

    create the file for my agent

---
+++ langgraph/email_agent.py
@@ -0,0 +1,17 @@
+import os
+from typing import TypedDict, List, Dict, Any, Optional
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage
+
+
+# Define the State
+
+# Definethe nodes
+
+# Edge:
+
+# StateGraph:
+
+# Visualisation
+
+# Invokation
