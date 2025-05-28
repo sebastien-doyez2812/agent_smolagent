68983130b66e0cac749fbb5c278ad00a5e489a72
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-27 21:55:54 -0400

    add the state

--- langgraph/email_agent.py
+++ langgraph/email_agent.py
@@ -2,11 +2,21 @@ import os
 from typing import TypedDict, List, Dict, Any, Optional
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import HumanMessage
-
+from langchain_ollama import OllamaLLM
 
 # Define the State
+class State(TypedDict):
+    email: Dict[str, Any]
+    email_category : Optional[str]
+    spam_reason : Optional[str]
+    is_spam: Optional[bool]
+    email_draft:Optional[str]
+    messages: List[Dict[str, Any]]
+
 
 # Definethe nodes
+model = OllamaLLM(name= "gemma")
+
 
 # Edge:
 
