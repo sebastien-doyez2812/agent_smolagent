8dccd0cfab9e495f4e62e1ed46485fada8e7ef0c
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-29 21:28:15 -0400

    add node

--- langgraph/doc_analysis_graph.py
+++ langgraph/doc_analysis_graph.py
@@ -67,4 +67,28 @@ tools = [
 
 llm = ChatOllama(model= gemma)
 
-llm.bind_tools(tools= tools)
+llm_with_tools = llm.bind_tools(tools= tools)
+
+# Node: 
+def assistant (state: AgentState):
+    # Description for the tools:
+    textual_description_of_tool="""
+        extract_text(img_path: str) -> str:
+            Extract text from an image file using a multimodal model.
+
+            Args:
+                img_path: A local image file path (strings).
+
+            Returns:
+                A single string containing the concatenated text extracted from each image.
+        divide(a: int, b: int) -> float:
+            Divide a and b
+        """
+    image = state["input_file"]
+    sys_msg = SystemMessage(content=f"You are a helpful butler named Assistant. You can analyse documents and run computations with provided tools:\n{textual_description_of_tool} \n You have access to some optional images. Currently the loaded image is: {image}")
+
+
+    return {
+        "messages": [llm_with_tools.invoke([sys_msg] + state["messages"])],
+        "input_file": state["input_file"]
+    }
