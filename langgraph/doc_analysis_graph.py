ca3ff113f30099a8be120d4589dd7c92cd244716
sebastien doyez <doyez.sebastien34090@gmail.com>
2025-05-29 21:13:44 -0400

    add the agentstate

---
+++ langgraph/doc_analysis_graph.py
@@ -0,0 +1,15 @@
+import base64
+from typing import List, TypedDict, Annotated, Optional
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+from IPython.display import Image, display
+
+
+
+
+class AgentState(TypedDict):
+    input_file: Optional[str]
+    messages: Annotated[list[AnyMessage], add_messages]
